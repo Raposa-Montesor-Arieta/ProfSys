4fc401a425715ae3fe1bf289c71212d7e3862a29
Christian Montesor <christianmontesor122@gmail.com>
2023-09-05 15:33:05 +0800

    insert with img

--- ProfSys/form.php
+++ ProfSys/form.php
@@ -1,4 +1,3 @@
-
 <?php
 include 'connection.php';
 ?>
@@ -15,63 +14,70 @@ include 'connection.php';
 
     <nav>
         <div class="nav-left">
-            <img src="logo.png" class="logo">
+            <img src="image/logo.png" class="logo">
             <ul>
-                <li><img src="home.png"></li>
-                <li><img src="inbox.png"></li>
+                <li><img src="image/home.png"></li>
+                <li><img src="image/inbox.png"></li>
             </ul>
         </div>
         <div class="nav-right">
 
             <div class="search-box">
-                <img src="search.png">
+                <img src="image/search.png">
                 <input type="text" placeholder="Search"> 
             </div>
             <div class="nav-user-icon online">
-                <img src="profile.png">
+                <img src="image/profile.jpg">
             </div>
         </div>
     </nav>
-
-    <div class="container">
-        <h2>Student Information Form</h2>
-        <form action="inserts.php" method="POST">
-            <div class="form-columns">
-                <div class="column left-column">
-                    <div class="form-group">
-                        <label for="first_name">First Name:</label>
-                        <input type="text" id="first_name" name="first_name" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="middle_name">Middle Name:</label>
-                        <input type="text" id="middle_name" name="middle_name">
-                    </div>
-                    <div class="form-group">
-                        <label for="last_name">Last Name:</label>
-                        <input type="text" id="last_name" name="last_name" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="age">Age:</label>
-                        <input type="number" id="age" name="age" required>
-                    </div>
+    <form action="inserts.php" method="POST" enctype="multipart/form-data">
+    <div class="container-box">
+        <h2>Professor Information Form</h2>
+        <div class="form-columns">
+            <div class="column">
+                <div class="form-group">
+                    <label for="first_name">First Name:</label>
+                    <input type="text" id="first_name" name="first_name" required>
                 </div>
-                <div class="column right-column">
-                    <div class="form-group">
-                        <label for="contact_number">Contact Number:</label>
-                        <input type="text" id="contact_number" name="contact_number" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="email">Email:</label>
-                        <input type="email" id="email" name="email" required>
-                    </div>
-                    <div class="form-group">
-                        <label for="textarea">Educational Background:</label>
-                        <textarea id="textarea" name="textarea" rows="4" cols="50"></textarea>
-                    </div>
+                <div class="form-group">
+                    <label for="middle_name">Middle Name:</label>
+                    <input type="text" id="middle_name" name="middle_name">
+                </div>
+                <div class="form-group">
+                    <label for="last_name">Last Name:</label>
+                    <input type="text" id="last_name" name="last_name" required>
+                </div>
+                <div class="form-group">
+                    <label for="age">Age:</label>
+                    <input type="number" id="age" name="age" required>
+                </div>
+                <div class="form-group">
+                    <label for="contact_number">Contact Number:</label>
+                    <input type="text" id="contact_number" name="contact_number" required>
                 </div>
             </div>
-            <button type="submit" name="submit" value="submit">Submit</button>
-        </form>
+            <div class="column">
+                <div class="form-group">
+                    <label for="address">Address:</label>
+                    <input type="text" id="address" name="address" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" id="email" name="email" required>
+                </div>
+                <div class="form-group">
+                    <label for="file_upload">Upload Picture:</label>
+                    <input type="file" id="file_upload" name="img" accept="image/*" required>
+                </div>
+                <div class="form-group">
+                    <label for="textarea">Educational Attainment:</label>
+                    <textarea id="textarea" name="textarea" rows="4" cols="50"></textarea>
+                </div>
+            </div>
+        </div>
+        <button type="submit" name="submit" value="submit">Submit</button>
     </div>
+</form>
 </body>
 </html>
--- ProfSys/inserts.php
+++ ProfSys/inserts.php
@@ -1,20 +1,45 @@
 <?php
-    include "connection.php";
+include "connection.php";
 
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $first_name = $_POST["first_name"];
     $middle_name = $_POST["middle_name"];
     $last_name = $_POST["last_name"];
     $age = $_POST["age"];
     $contact_number = $_POST["contact_number"];
+    $address = $_POST["address"];
     $email = $_POST["email"];
     $educational_background = $_POST["textarea"];
 
-    $sql = "INSERT INTO `profs`(`first_name`, `mid_name`, `last_name`, `age`,`contact_num`, `email`, `educ`) 
-    VALUES ('$first_name','$middle_name','$last_name','$age','$contact_number','$email', '$educational_background')";
+    // Handling image upload
+    $img_name = $_FILES['img']['name'];
+    $img_tmp = $_FILES['img']['tmp_name'];
+    $img_dir = "uploads/"; // Specify your image upload directory
+    $img_path = $img_dir . $img_name;
 
+    // Check if the file was uploaded successfully
+    if (is_uploaded_file($img_tmp)) {
+        if (move_uploaded_file($img_tmp, $img_path)) {
+            // Image uploaded successfully, proceed with database insert
 
-    $conn -> query($sql) or die ($conn->error);
-    echo header("Location: normal-tables.php");
+            // SQL statement to insert data with the image
+            $sql = "INSERT INTO `profs`(`first_name`, `mid_name`, `last_name`, `age`, `contact_num`, `address`, `email`, `img`, `educ`) 
+            VALUES ('$first_name','$middle_name','$last_name','$age','$contact_number', '$address','$email', '$img_name', '$educational_background')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "<script>alert('CREATED SUCCESSFULLY'); </script>";
+                echo "<script>window.location.assign('form.php')</script>";
+            } else {
+                echo "<script>alert('Error: " . $conn->error . "');</script>";
+                echo "<script>window.location.assign('form.php')</script>";
+            }
+        } else {
+            echo "<script>alert('Error moving uploaded image to directory'); </script>";
+            echo "<script>window.location.assign('form.php')</script>";
+        }
+    } else {
+        echo "<script>alert('Error uploading image'); </script>";
+        echo "<script>window.location.assign('form.php')</script>";
+    }
 }
 ?>
