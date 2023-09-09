0b6afd9f05a92b25034344ef6a5e04fc3fe7e003
jaderaposa <jaduya678@gmail.com>
2023-09-09 14:38:34 +0800

    remember me function, not working yet

--- ProfSys/log-in.php
+++ ProfSys/log-in.php
@@ -47,6 +47,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style1.css">
     <link rel="stylesheet" href="boxicons/css/boxicons.css">
+    <script src="js/rememberme.js" type="text/javascript"></script>
     <title>Ludiflex | Login</title>
 </head>
 <body>
@@ -82,8 +83,8 @@
             </div>
             <div class="bottom">
                 <div class="left">
-                    <input type="checkbox"  id="check">
-                    <label for="check"> Remember Me</label>
+                    <input type="checkbox" name="rememberMe" value="true" id="rememberMe">
+                    <label for="rememberMe">Remember Me</label>
                 </div>
                 <div class="right">
                     <label><a href="#">Forgot password?</a></label>
