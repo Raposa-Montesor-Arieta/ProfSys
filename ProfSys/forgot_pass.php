68c916990783c057e5445dd1783725e82b523044
jaderaposa <jaduya678@gmail.com>
2023-09-09 20:42:06 +0800

    deletion and adjustment, forgot password not done yet

--- ProfSys/forgot_pass.php
+++ ProfSys/forgot_pass.php
@@ -10,11 +10,11 @@
     <div class="container">
         <h2>Forgot Password</h2>
         <form action="#" method="post">
-            <label for="email">Please enter your username:</label>
+            <label for="email">Please Enter your E-mail:</label>
             <input type="email" id="email" name="email" placeholder=" Enter your email">
             <div class="kkk">
-            <button type="submit"><a href="log-in.php" style="text-decoration: none; color: #fff;">Cancel</a></button>
-            <button type="submit">Reset Password</button>
+            <button type="submit"><a href="log-in.php" style="text-decoration: none;color: black;">Cancel</a></button>
+            <button type="submit" style="text-decoration: none;color: black;">Send</button>
             </div>
         </form>
     </div>
